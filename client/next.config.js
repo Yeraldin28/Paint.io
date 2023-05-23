bf8f43c870620d5cace51e550d5336d2a7747ffb
ypulido28 <ypulido46@misena.edu.co>
2023-05-23 16:02:15 -0500

    [UPDATE] next-compose-plugins

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,10 @@ const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withTM = require('next-transpile-modules')(['tailwindcss']);
 
-module.exports = withPlugins([withCSS, withTM], {
+const nextConfig = {
   experimental: {
     appDir: true,
   },
-});
+};
 
+module.exports = withPlugins([withCSS, withTM], nextConfig);
