85fdaa454efc9b45966f3485ed263be8e574c4d8
ypulido28 <ypulido46@misena.edu.co>
2023-05-24 16:17:56 -0500

    [UPDATE]funciona?7

--- client/app/room/[id]/page.tsx
+++ client/app/room/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import React from 'react';
 import { useEffect, useRef, useState } from "react"
 import useDraw from "../../../hooks/useDraw"
 import { CirclePicker } from "react-color"
@@ -141,7 +141,7 @@ const Room = ({ params }: ParamsProps) => {
                             </h1>
                             <CirclePicker
                                 color={color}
-                                onChange={(e) => setColor(e.hex)}
+                                onChange={(e:any) => setColor(e.hex)}
                             />
                         </div>
                         {/*         Size Picker     */}
